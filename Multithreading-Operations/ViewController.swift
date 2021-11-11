0a7e176e5f292ed16da44ac6f03def7da7fe49eb
Rus <de.mr44@gmail.com>
2021-11-11 14:54:27 +0500

    Added the image downloading function

--- Multithreading-Operations/ViewController.swift
+++ Multithreading-Operations/ViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+// practicing with the operation queues
+
 final class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
@@ -14,11 +16,9 @@ final class ViewController: UIViewController {
     @IBOutlet weak var downloadButton: UIButton!
     @IBOutlet weak var cancelButton: UIButton!
     
-    private let queue = OperationQueue()
-    private let imageURLs: [URL] = [
-        URL(string: "https://images.unsplash.com/photo-1635813782590-0f5d34934b9d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80")!,
-        URL(string: "https://images.unsplash.com/photo-1635372886281-4487e1e00904?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80")!
-    ]
+    private let mainQueue = OperationQueue.main
+    private let concurrentQueue = OperationQueue()
+        
     private var timer = Timer()
     private var timerCount = 3 {
         didSet {
@@ -26,16 +26,15 @@ final class ViewController: UIViewController {
         }
     }
     
-    private var image: UIImage?
+    private let imageURLs: [URL] = [
+        URL(string: "https://images.unsplash.com/photo-1635813782590-0f5d34934b9d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1287&q=80")!,
+        URL(string: "https://images.unsplash.com/photo-1635372886281-4487e1e00904?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2274&q=80")!
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configuringUI()
-        
-        setBlockOperation {
-            self.image = self.getImage(from: self.imageURLs[0])
-        }
     }
     
     // configuring UI
@@ -47,6 +46,28 @@ final class ViewController: UIViewController {
         cancelButton.layer.cornerRadius = 15
     }
     
+    // async downloading an image and setting it up in the main thread
+    private func downloadImage(from URL: URL) {
+        var image = UIImage()
+        let getImageOperation = BlockOperation {
+            image = self.getImage(from: URL)
+        }
+        let setImageOperation = BlockOperation {
+            self.imageView.image = image
+        }
+        setImageOperation.addDependency(getImageOperation)
+        concurrentQueue.addOperation(getImageOperation)
+        mainQueue.addOperation(setImageOperation)
+    }
+    
+    // trying to get an image
+    private func getImage(from URL: URL) -> UIImage {
+        guard let data = try? Data(contentsOf: URL), let image = UIImage(data: data) else {
+            return UIImage(systemName: "nosign")!
+        }
+        return image
+    }
+        
     // setting up the timer
     private func setUpTimer() {
         label.isHidden = false
@@ -66,24 +87,9 @@ final class ViewController: UIViewController {
         cancelButton.isEnabled = false
     }
     
-    // creation an operation block
-    private func setBlockOperation(_ block: @escaping () -> Void) {
-        let blockOperation = BlockOperation {
-            block()
-        }
-        queue.addOperation(blockOperation)
-    }
-    
-    // trying to get an image
-    private func getImage(from URL: URL) -> UIImage? {
-        guard let data = try? Data(contentsOf: URL) else { return nil }
-        let image = UIImage(data: data)
-        
-        return image
-    }
-    
     @IBAction func downloadButtonAction(_ sender: UIButton) {
-        
+        imageView.image = nil
+        downloadImage(from: imageURLs[0])
     }
     
     @IBAction func cancelButtonAction(_ sender: UIButton) {
